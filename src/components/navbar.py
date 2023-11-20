c1aa6fc4e25f18aaf706be33fd3f324ea531cff8
Guido Cioni <guido.cioni@airbus.com>
2023-11-20 16:44:36 +0100

    Automatically collapse navbar when NavLink clicked

--- src/components/navbar.py
+++ src/components/navbar.py
@@ -7,7 +7,7 @@ https://dash-bootstrap-components.opensource.faculty.ai/docs/components/navbar/
 
 # package imports
 import dash
-from dash import callback, Output, Input, State
+from dash import callback, Output, Input, State, ALL
 import dash_bootstrap_components as dbc
 
 
@@ -22,6 +22,7 @@ def navbar():
                             dbc.NavItem(
                                 dbc.NavLink(
                                     page['title'],
+                                    id={'type': 'navbar-link', 'index': page["relative_path"].split("/")[-1]},
                                     href=page["relative_path"]
                                 )
                             ) for page in dash.page_registry.values()
@@ -48,3 +49,13 @@ def toggle_navbar_collapse(n, is_open):
     if n:
         return not is_open
     return is_open
+
+
+@callback(
+    Output('navbar-collapse', 'is_open', allow_duplicate=True),
+    Input({'type': 'navbar-link', 'index': ALL}, 'n_clicks'),
+    State('navbar-collapse', 'is_open'),
+    prevent_initial_call=True
+)
+def toggle_navbar_collapse_from_navlink(navlink_clicks, is_open):
+    return False if is_open else any(navlink_clicks)
