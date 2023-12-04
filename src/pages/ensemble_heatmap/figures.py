58e5b8a432778a7fa80fc5be2f51f5897e50c873
Guido Cioni <guido.cioni@airbus.com>
2023-12-04 11:20:15 +0100

    Improved zooming on heatmap Improved tooltips on heatmap

--- src/pages/ensemble_heatmap/figures.py
+++ src/pages/ensemble_heatmap/figures.py
@@ -26,15 +26,15 @@ def make_heatmap(df, var, title=None):
         color_continuous_scale=cmap,
         origin='lower')
 
+    fig.update_traces(hovertemplate="<extra></extra><b>%{x|%a %d %b %H:%M}</b><br>%{y}<br>Value = %{z}")
+
     fig.update_yaxes(visible=False, showticklabels=False)
 
     fig.update_layout(
-        xaxis=dict(showgrid=True,
-                   range=[df['time'].min(), df['time'].max()],
-                   tickformat='%a %d %b\n%H:%M'),
-        yaxis=dict(showgrid=True),
+        xaxis=dict(showgrid=True, tickformat='%a %d %b\n%H:%M'),
+        yaxis=dict(showgrid=True, fixedrange=True),
         height=700,
-        margin={"r": 5, "t": 30, "l": 5, "b": 5},
+        margin={"r": 5, "t": 40, "l": 5, "b": 5},
     )
 
     fig.update_coloraxes(showscale=False)
